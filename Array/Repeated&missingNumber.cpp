6845881c1fb8829c8f62441ff9e393d140c98456
hemant <hemantvijawat2001@gmail.com>
2026-09-11 22:50:23 +0530

    added optmial approach

--- Array/Repeated&missingNumber.cpp
+++ Array/Repeated&missingNumber.cpp
@@ -24,3 +24,26 @@ public:
       return {repeated,missing};
     }
 };
+
+
+ /*
+ class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& arr) {
+      int n =arr.size();
+      vector<int> freq(n*n+1,0);
+      int repeated = -1;
+      int missing = -1;
+      for(int i =0;i<n;i++){
+        for(int j=0;j<n;j++){
+            freq[arr[i][j]]++;
+            }
+        }
+        for(int i=0;i<=n*n;i++){
+            if(freq[i] == 2) repeated = i;
+            else if(freq[i] == 0 ) missing = i;
+        }  
+      return {repeated,missing};
+    }
+}; 
+*/
