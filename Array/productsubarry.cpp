34605b404349b470646c4b12bc6f0f7dab20e65c
hemant <hemantvijawat2001@gmail.com>
2026-09-11 23:32:10 +0530

    added optimal approach

--- Array/productsubarry.cpp
+++ Array/productsubarry.cpp
@@ -14,3 +14,27 @@ public:
         return maxi;
     }
 };
+
+/*
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int CntMax = nums[0];
+        int CntMin = nums[0];
+        int maxi = nums[0];
+        for(int i=1;i<n;i++){
+            int X = nums[i];
+            int OMax = CntMax;
+            int OMin = CntMin;
+            CntMax = max({X,X*OMax,X*OMin});
+            CntMin = min({X,X*OMax,X*OMin}); 
+            maxi = max(maxi , CntMax);
+
+        }
+        return maxi;
+    }
+};
+*/
+
+// this it the optimal solution that we have added in this int Cntmax = max({x , x*omax, x * omin})
