fda28f3462fd4fa1e7744d7d5e0b98ac8c6af97c
hemant <hemantvijawat2001@gmail.com>
2026-09-18 00:03:34 +0530

    added

---
+++ BinarySearch/kokoEatingBananas.cpp
@@ -0,0 +1,36 @@
+
+class Solution {
+public:
+int  findmax(vector<int> & v){
+ int maxi = INT_MIN;
+ int n = v.size();
+ for(int i=0;i<n;i++){
+   maxi = max(maxi , v[i]);  
+ } 
+ return maxi;
+}  
+  int calculateTotalHours(vector<int> & v , int hour){
+    int totalH =0;
+    int n = v.size();
+    for(int i=0;i<n;i++){
+        totalH += ceil((double)v[i] / (double)hour);
+    }
+    return totalH;
+  }
+
+
+    int minEatingSpeed(vector<int>& v, int h) {
+        int low = 1;
+        int high = findmax(v);
+        while(low <= high){
+            int mid = (low+high)/2;
+            int totalH = calculateTotalHours(v , mid);
+            if(totalH <= h){
+                high  = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
